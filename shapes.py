11587298e1628230f66c5e2076054628e14c638a
OmarGomaa27 <omargomaa27@gmail.com>
2025-10-22 18:14:29 -0400

    added class Circle(Shape)

--- shapes.py
+++ shapes.py
@@ -14,3 +14,26 @@ class Shape:
     
     def __str__(self):
         return f"{self.name}"
+    
+class Circle(Shape):
+    """Class representing a circle."""
+    
+    def __init__(self, name, radius):
+        super().__init__(name)
+        self.radius = radius
+
+    def area(self):
+        return 3.14159 * (self.radius ** 2)
+
+    def perimeter(self):
+        return 2 * 3.14159 * self.radius
+
+    def __str__(self):
+        return f"Circle(radius={self.radius})"
+    
+
+    #   ---Circle Test---
+circle = Circle("MyCircle", 5)
+print(circle.area())      # 78.53975
+print(circle.perimeter()) # 31.4159
+print(circle)             # Circle(radius=5)
